bbbde5f92710a3705a0da7456928c3262b2f28a0
MuhametSmaili <muhamet5@msn.com>
2022-04-02 16:38:55 +0300

    Added: 	Plugins for linting, and some basic linting rules 	chrome type support (global obj of chrome)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
