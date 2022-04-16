343175011e1dadc955971f8d509d320ad552b579
bkrmadtya <karki.bikram007@gmail.com>
2022-04-16 17:54:56 +0300

    feat: added keyBindingFucntion and refactored logic

--- components/NoteEditor.tsx
+++ components/NoteEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
-import { EditorProps, EditorState, RichUtils } from 'draft-js';
+import { EditorProps, EditorState, getDefaultKeyBinding, KeyBindingUtil, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 const Editor = dynamic<EditorProps>(() => import('draft-js').then((mod) => mod.Editor), { ssr: false });
@@ -10,23 +10,23 @@ const NoteEditor = () => {
 
   // For keyboard shortcuts
   const handleKeyCommand = (command: string, _: EditorState, __: number) => {
+    const commandMap: Map<string, string> = new Map([
+      ['strikethrough', 'STRIKETHROUGH'],
+      ['ordered-list', 'ordered-list-item'],
+      ['unordered-list', 'unordered-list-item'],
+      ['blockquote', 'blockquote'],
+    ]);
+
+    const utilsFn = (command: string) =>
+      command === 'strikethrough' ? RichUtils.toggleInlineStyle : RichUtils.toggleBlockType;
+
     // inline formatting key commands handles bold, italic, code, underline
     let newEditorState: EditorState | null = RichUtils.handleKeyCommand(editorState, command);
 
-    if (!newEditorState && command === 'strikethrough') {
-      newEditorState = RichUtils.toggleInlineStyle(editorState, 'STRIKETHROUGH');
-    }
+    const style = commandMap.get(command);
 
-    if (!newEditorState && command === 'blockquote') {
-      newEditorState = RichUtils.toggleBlockType(editorState, 'blockquote');
-    }
-
-    if (!newEditorState && command === 'ordered-list') {
-      newEditorState = RichUtils.toggleBlockType(editorState, 'ordered-list-item');
-    }
-
-    if (!newEditorState && command === 'unordered-list') {
-      newEditorState = RichUtils.toggleBlockType(editorState, 'unordered-list-item');
+    if (!newEditorState && style) {
+      newEditorState = utilsFn(command)(editorState, style);
     }
 
     if (newEditorState) {
@@ -35,6 +35,23 @@ const NoteEditor = () => {
     }
 
     return 'not-handled';
+  };
+
+  const keyBindingFunction = (event: React.KeyboardEvent) => {
+    const keyMap: Map<number | string, string> = new Map([
+      ['x', 'strikethrough'],
+      ['7', 'ordered-list'],
+      ['8', 'unordered-list'],
+      ['9', 'blockquote'],
+    ]);
+
+    const key = keyMap.get(event.key);
+
+    if (key && KeyBindingUtil.hasCommandModifier(event) && event.shiftKey) {
+      return key;
+    }
+
+    return getDefaultKeyBinding(event);
   };
 
   return (
@@ -52,6 +69,7 @@ const NoteEditor = () => {
         <Editor
           editorState={editorState}
           handleKeyCommand={handleKeyCommand}
+          keyBindingFn={keyBindingFunction}
           onChange={setEditorState}
           placeholder="Note down your ideas..."
         />
