cb114946fcddc92604b54495ef9f23a9e5c24f4d
bkrmadtya <karki.bikram007@gmail.com>
2022-04-16 17:34:44 +0300

    feat: added handleKeyCommand method

--- components/NoteEditor.tsx
+++ components/NoteEditor.tsx
@@ -1,12 +1,41 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
-import { EditorProps, EditorState } from 'draft-js';
+import { EditorProps, EditorState, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 const Editor = dynamic<EditorProps>(() => import('draft-js').then((mod) => mod.Editor), { ssr: false });
 
 const NoteEditor = () => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+  // For keyboard shortcuts
+  const handleKeyCommand = (command: string, _: EditorState, __: number) => {
+    // inline formatting key commands handles bold, italic, code, underline
+    let newEditorState: EditorState | null = RichUtils.handleKeyCommand(editorState, command);
+
+    if (!newEditorState && command === 'strikethrough') {
+      newEditorState = RichUtils.toggleInlineStyle(editorState, 'STRIKETHROUGH');
+    }
+
+    if (!newEditorState && command === 'blockquote') {
+      newEditorState = RichUtils.toggleBlockType(editorState, 'blockquote');
+    }
+
+    if (!newEditorState && command === 'ordered-list') {
+      newEditorState = RichUtils.toggleBlockType(editorState, 'ordered-list-item');
+    }
+
+    if (!newEditorState && command === 'unordered-list') {
+      newEditorState = RichUtils.toggleBlockType(editorState, 'unordered-list-item');
+    }
+
+    if (newEditorState) {
+      setEditorState(newEditorState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  };
 
   return (
     <>
@@ -20,7 +49,12 @@ const NoteEditor = () => {
           overflowY: 'auto',
         }}
       >
-        <Editor editorState={editorState} onChange={setEditorState} placeholder="Note down your ideas..." />
+        <Editor
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          onChange={setEditorState}
+          placeholder="Note down your ideas..."
+        />
       </div>
     </>
   );
