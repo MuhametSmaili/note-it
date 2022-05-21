e7ea0a057a740055a9284f6ada0835cf7b518a33
MuhametSmaili <muhamet5@msn.com>
2022-05-21 19:52:40 +0300

    refactor: ♻ added the basic folders for the extesnion and moved the folders into src

---
+++ src/background/background.ts
@@ -0,0 +1,1 @@
+console.log('BACKGROUND script');
---
+++ src/popup/popup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { rootRender } from 'src/utils/render';
+
+const App: React.FC = () => {
+  return <div>Hello</div>;
+};
+
+rootRender.render(<App />);
