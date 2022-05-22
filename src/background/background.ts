2828280e50c26a0ab2e19ea37620d01b17486d7b
MuhametSmaili <muhamet5@msn.com>
2022-05-22 21:53:16 +0300

    feat: ✨ added crop area with react

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,16 @@
-chrome.runtime.onInstalled.addListener(() => {});
+import { MessageRequest } from '@utils/MessageRequest';
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  if (request.message === MessageRequest.CROP_SCREEN) {
+    chrome.scripting.executeScript({
+      target: { tabId: Number(request.tab.id) },
+      files: ['contentScript.js'],
+    });
+    return true;
+  } else if (request.message === MessageRequest.CAPTURE_SCREEN) {
+    chrome.tabs.captureVisibleTab(chrome.windows.WINDOW_ID_CURRENT, {}, function (dataUrl) {
+      response({ imgSrc: dataUrl });
+    });
+    return true;
+  }
+});
