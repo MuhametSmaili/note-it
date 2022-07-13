70fa3def1634a13a8d3cff32debe0340f4e611d1
MuhametSmaili <muhamet5@msn.com>
2022-07-13 17:02:00 +0300

    feat: ✨ addec context menu for opening in window/poup

---
+++ src/background/contextMenu.ts
@@ -0,0 +1,26 @@
+import { setStorage } from '@utils/storage';
+
+chrome.contextMenus.create({
+  id: 'popup',
+  type: 'radio',
+  title: 'Open in popup',
+  contexts: ['action'],
+  checked: true,
+});
+
+chrome.contextMenus.create({
+  id: 'window',
+  type: 'radio',
+  title: 'Open in window',
+  contexts: ['action'],
+  checked: false,
+});
+
+chrome.contextMenus.onClicked.addListener((e) => {
+  setStorage({ activeMenu: e.menuItemId as 'popup' | 'window' });
+  if (e.menuItemId === 'popup') {
+    chrome.action.setPopup({ popup: 'popup.html' });
+  } else {
+    chrome.action.setPopup({ popup: '' });
+  }
+});
