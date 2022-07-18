735512998fe25d56188630bca8382795aae906b7
MuhametSmaili <muhamet5@msn.com>
2022-07-18 19:40:54 +0300

    feat: ✨ added icons path to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -11,5 +11,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/config/test-setup.ts',
+  },
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      '@icons': path.resolve('/src/icons'),
+    },
   },
 });
