3a75e7919e87ed296742208014c8362f0ec3be92
MuhametSmaili <muhamet5@msn.com>
2022-07-31 17:52:16 +0300

    refactor: 🛠  renamed name of activeTab to activeWindow

--- src/background/activeWindow.ts
+++ src/background/activeWindow.ts
@@ -3,9 +3,9 @@ import { setStorage } from '@utils/storage';
 // active tab changed
 // Needs to save last two active tabs
 // so we can access the last active tab and not the extension tab
-let activeTabs: Array<{ windowId: number }> = [];
+let activeWindows: Array<{ windowId: number }> = [];
 chrome.windows.onFocusChanged.addListener((windowId) => {
-  activeTabs = activeTabs.slice(0, 1);
-  activeTabs.unshift({ windowId });
-  setStorage({ activeWindow: activeTabs });
+  activeWindows = activeWindows.slice(0, 1);
+  activeWindows.unshift({ windowId });
+  setStorage({ activeWindow: activeWindows });
 });
